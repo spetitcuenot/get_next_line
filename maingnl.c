29f2d2b2a246e102d5c388fa4b95fd204510bd56
spetitcuenot <spetitcuenot@magic.fr>
2018-12-31 17:27:45 +0100

    maingnl

--- maingnl.c
+++ maingnl.c
@@ -6,7 +6,7 @@
 /*   By: spetitcu <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/12/11 16:03:59 by spetitcu     #+#   ##    ##    #+#       */
-/*   Updated: 2018/12/11 16:13:59 by spetitcu    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/12/31 17:27:06 by spetitcu    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -20,39 +20,6 @@
 #include <unistd.h>
 
 int	get_next_line(const int fd, char **line);
-/*{
-	//(void)line;
-	t_list	*lst;
-	int		i;
-	t_list 	*elem;
-	char	*str;
-	static char *line;
-	unsigned char	buff[BUFF_SIZE];
-	
-	i = 0;
-	ft_putstr("avant read\n");
-	while ((read(fd, &buff, BUFF_SIZE + 1) > 0))
-	{
-		elem = ft_lstnew(buff, BUFF_SIZE + 1);
-		ft_lstaddend(&lst, elem);
-	}
-	ft_lstprint(lst);
-	
-	while (lst)
-	{
-		str = lst->content;
-		while (str[i])
-			if (str[i] == "\n")
-			line[i] = str;
-		
-
-		lst = lst->next;
-		i++;
-	}
-	line[i] = '\0';
-	return (0);
-}
-*/
 
 int main(int argc, char **argv)
 {
